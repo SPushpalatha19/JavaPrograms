c49290a709f0c6b28a094875412e4f3fa7098864
SPushpalatha19 <131225863+SPushpalatha19@users.noreply.github.com>
2023-07-20 21:44:47 +0530

    Delete day6 directory

--- day6/AreaOfTheSquare.java
+++
@@ -1,9 +0,0 @@
-class AreaOfTheSquare
-  {
-    public static void main(String args[])
-    {
-      float side=23.30f,area;
-      area=side*side;
-      System.out.println("The area of the square is "+area);
-    }
-  }
